023e5950412d67d7815ec27c9ac1c7aa0a7be8b1
Diederick C. Niehorster <dcnieho@gmail.com>
2018-12-22 22:06:08 +0100

    split coding file loading off into separate function

--- function_library/getCodingData.m
+++ function_library/getCodingData.m
@@ -106,33 +106,10 @@ for p=1:nStream
             % if nothing there yet, or always reload option set, load from
             % file
             if isscalar(coding.mark{p}) || (isfield(coding.stream.options{p},'alwaysReload') && coding.stream.options{p}.alwaysReload)
-                fname   = getFullPath(coding.stream.options{p}.file);
-                codeF   = fileread(fname);
-                codeF   = reshape(sscanf(codeF,'%f'),2,[]);     % format: lines with [event start, event type]. this is sufficient as assumption of this code is that every sample is tagged with an event type. use event "none" or "other" for things that are not of interest to you
-                ts      = codeF(1,:);
-                type    = codeF(2,:);
-                % add start of time
-                if ts(1)>1
-                    ts  = [1 ts];
-                    if type(1)==1
-                        type= [2 type];
-                    else
-                        type= [1 type];
-                    end
-                end
-                % add end
-                if ts(end)>=endT
-                    % last mark at last sample: this does not start a new
-                    % event anymore (always one more mark than type as
-                    % marks also needed to close off events)
-                    type(end) = [];
-                    ts(end) = endT; % ensure end not beyond data
-                else
-                    ts = [ts endT];
-                end
+                tempCoding = loadCodingFile(coding.stream.options{p},endT);
                 % store
-                coding.mark{p} = ts;
-                coding.type{p} = type;
+                coding.mark{p} = tempCoding.mark;
+                coding.type{p} = tempCoding.type;
             end
         case 'classifier'
     end
