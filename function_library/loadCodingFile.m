1de5fad392312030715bbe007a00df752655ad79
Diederick C. Niehorster <dcnieho@gmail.com>
2019-02-02 11:02:29 +0100

    loadCodingFile: must always be called with end time

--- function_library/loadCodingFile.m
+++ function_library/loadCodingFile.m
@@ -4,11 +4,8 @@ codeF   = fileread(fname);
 codeF   = reshape(sscanf(codeF,'%f'),2,[]);     % format: lines with [event start, event type]. this is sufficient as assumption of this code is that every sample is tagged with an event type. use event "none" or "other" for things that are not of interest to you
 ts      = codeF(1,:);
 type    = codeF(2,:);
-if nargin>1
-    [ts,type] = addStartEndCoding(ts,type,endT);
-else
-    [ts,type] = addStartEndCoding(ts,type);
-end
+% fix up
+[ts,type] = addStartEndCoding(ts,type,endT);
 % store
 coding.mark = ts;
 coding.type = type;
