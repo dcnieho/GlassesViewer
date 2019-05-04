2e3f0f53162c7747ac21a5139228ff8e855f06e2
Diederick C. Niehorster <dcnieho@gmail.com>
2019-05-04 14:07:27 +0200

    getDPIScale: now has useful advice when function errors because matlab needs to be restarted

--- function_library/getDPIScale.m
+++ function_library/getDPIScale.m
@@ -2,5 +2,5 @@ function scale = getDPIScale()
 jScreen = java.awt.Toolkit.getDefaultToolkit.getScreenSize; % real pixels
 mScreen = get(0,'ScreenSize');                              % DPI-scaled pixels
 scale = [jScreen.width jScreen.height]./mScreen(3:4);
-assert(isequal(scale(1),scale(2)))
+assert(isequal(scale(1),scale(2)),'Restart matlab while the screen you want to run this GUI on is already attached (problem figuring out display scaling)')
 scale = scale(1);
