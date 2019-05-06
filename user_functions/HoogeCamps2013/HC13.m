bbb052910c413453aa9430560ebd35ca7accee03
Diederick C. Niehorster <dcnieho@gmail.com>
2019-05-06 15:21:47 +0200

    both example event classifiers now output correct starting marks, and also code events as "none" instead of "fast" if most data is missing during the episode

--- user_functions/HoogeCamps2013/HC13.m
+++ user_functions/HoogeCamps2013/HC13.m
@@ -1,4 +1,4 @@
-function [emark,type] = HC13(tobiiData,params)
+function [smarks,type] = HC13(tobiiData,params)
 
 % NB! classifier function should output event mark times in original times
 % as passed in here, so t=0 should correspond to the same sample as what
@@ -13,15 +13,21 @@ v       = hypot(vx,vy);
 %%%%% detect slow phases
 emark   = HC13_detectfixaties2015(v,params,time);
 
-% turn back to seconds. Otherwise we're fine with this one, it keeps the
-% input time signal intact
-emark   = emark/1000;
-% this denotes slow event, now add fast in and turn into expected output
-% format
+%%%%% prep output
+% "emark" denotes slow events, now add fast in and turn into expected
+% output format
 % events are denoted as interleaved start and end times. we want only
 % starts, so take starts (uneven) and turn ends (even) into starts of other
 % event
-smarks  = sort([emark(1:2:end); emark(2:2:end)+1]);
+imarks  = find(ismember(time,emark));
+assert(length(imarks)==length(emark),'some output samples do not correspond to an input time. consider using findNearestTime, or fix algorithm')    % general advice, this should never fire for this algorithm
+imarks  = sort([imarks(1:2:end); imarks(2:2:end)+1]);
+
+% turn back to seconds. Otherwise we're fine with this one, it keeps the
+% input time signal intact
+time    = time/1000;
+smarks  = time(imarks);
+
 % defined categories are named with powers of 2, so in this case json
 % contains:
 % "categories": [ "none", 20, "slow", 4, "fast", 5 ]
@@ -30,6 +36,18 @@ smarks  = sort([emark(1:2:end); emark(2:2:end)+1]);
 % slow: 2
 % fast: 4
 
-% TODO: be bit smarter: if not slow phase because no data in interval, code
-% as 1, not 4
+% add fast in between the slow phases
 type    = repmat([2 4].',length(smarks)/2,1);
+
+% check each fast phase if actually missing (>50% of samples invalid), then
+% put code 1 instead ("none"). NB: this is specific to this classifier, you
+% may want something else for yours.
+% NB: this classifier always also eats one sample at either end of event,
+% so check for missing for 2:end-1. also, since one event end is other
+% event start, remove an extra sample from end for check
+for e=1:length(imarks)-1
+    idxs = imarks(e)+1:imarks(e+1)-2;
+    if sum(isnan(tobiiData.eye.binocular.gp(idxs,1)))>length(idxs)*.5
+        type(e) = 1;
+    end
+end
